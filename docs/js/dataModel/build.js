5bfb04ed0967f649645395e58539c4a397d7bcd8
Killian Lopez <killian.lopez@supdevinci-edu.fr>
2025-09-29 13:12:04 +0200

    update build

--- docs/js/dataModel/build.js
+++ docs/js/dataModel/build.js
@@ -16,87 +16,6 @@ const CLASS_LIST = {
 };
 
 const BUILD_DATA = {
-    "DPT Low Cost": {
-    "image": "assets/builds/DPT Low Cost.png",
-    "url": "https://zenithwakfu.com/builder/vvbog",
-    "keywords": ["Low Cost"],
-    "class": "Steamer",
-    "description": "Build orienté dégâts à distance, optimisé pour maximiser les coups critiques."
-    },
-    "Hupport Placeur": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 1": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 2": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 3": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 4": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 5": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    },
-    "Hupport 6": {
-        "image": "assets/builds/Hupport Placeur.png",
-        "url": "https://zenithwakfu.com/builder/g6wjy",
-        "keywords": ["Mid Cost"],
-        "class": "Huppermage",
-        "description": "Build axé sur le placement et le support d'équipe, avec une bonne capacité à twerker sur les alliés."
-    }
-,
-  "Cra Low Cost": {
-    "image": "assets/builds/Cra Low Cost.png",
-    "url": "https://zenithwakfu.com/builder/seeyu",
-    "keywords": ["Mid Cost"],
-    "class": "Feca",
-    "description": "test"
-  }
-,
-  "Iop Mid Cost": {
-    "image": "assets/builds/Iop Mid Cost.png",
-    "url": "https://zenithwakfu.com/builder/tp0dk",
-    "keywords": ["Mid Cost"],
-    "class": "Iop",
-    "description": "description"
-  }
-,
-  "tst": {
-    "image": "assets/builds/tst.png",
-    "url": "https://zenithwakfu.com/builder/tp0dk",
-    "keywords": ["Mid Cost"],
-    "class": "Feca",
-    "description": "tst"
-  }
-,
   "Ecaflip Support [Soin]": {
     "image": "assets/builds/Ecaflip Support [Soin].png",
     "url": "https://zenithwakfu.com/builder/1tk28",
@@ -107,3 +26,4 @@ const BUILD_DATA = {
 };
 
 export { BUILD_DATA, CLASS_LIST };
+
